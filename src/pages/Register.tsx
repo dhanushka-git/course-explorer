003a1314d63dea0abd286faf5415606ac75508bf
Shan-leo <bizwithshan@gmail.com>
2023-08-14 10:13:19 +0530

    feat: Login and sign up pages

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,132 @@
+import {
+    Box,
+    Button,
+    Flex,
+    FormControl,
+    FormLabel, Input,
+    InputGroup,
+    Select,
+    Stack,
+    useColorModeValue
+} from "@chakra-ui/react";
+import {useLocation, useNavigate} from "react-router-dom";
+import {useState} from "react";
+
 const Register = () => {
+    const {state} = useLocation();
+    const [id, setId] = useState();
+    const [email, setEmail] = useState();
+    const [isLoading, setIsLoading] = useState(false);
+    const [form, setForm] = useState<any>({});
+
+    const navigate = useNavigate();
+
+    const handleInputChange = (event: any) => {
+        const {name, value, type, checked} = event.target;
+    }
+
+    const onSubmitHandler = () => {
+        setIsLoading(true);
+        setTimeout(() => {
+            setIsLoading(false);
+            navigate("/");
+        }, 2000);
+    }
+
     return (
         <>
-            <h1>Register</h1>
+            <Flex
+                bg={useColorModeValue("blue.500", "blue.500")}
+                className={"w-[100vw] h-[100vh] justify-center flex-col align-center"}
+            >
+                <Stack
+                    flexDir="column"
+                    mb="2"
+                    justifyContent="center"
+                    alignItems="center"
+                >
+                    <Box className="" minW={{base: "90%", md: "468px"}}>
+                        <form>
+                            <Stack
+                                spacing={4}
+                                p="1rem"
+                                backgroundColor="whiteAlpha.900"
+                                boxShadow="md"
+                                className="rounded-xl"
+                            >
+                                <FormControl isRequired>
+                                    <FormLabel>Are you a Student or Institute</FormLabel>
+                                    <Select
+                                        placeholder="Who are you?"
+                                        color={"gray.800"}
+                                        name="type"
+                                        onChange={handleInputChange}
+                                    >
+                                        <option value={"student"}>Student</option>
+                                        <option value={"institute"}>Institute</option>
+                                    </Select>
+                                </FormControl>
+
+                                <FormControl isRequired>
+                                    <FormLabel>Name</FormLabel>
+                                    <InputGroup>
+                                        <Input
+                                            name="name"
+                                            onChange={handleInputChange}
+                                            type="text"
+                                            placeholder="Name"
+                                        />
+                                    </InputGroup>
+                                </FormControl>
+                                {form?.type === "student" ? (
+                                    <>
+                                        <FormControl>
+                                            <FormLabel>
+                                                Which category are you interested in?
+                                            </FormLabel>
+                                            <Select
+                                                placeholder="Select your interested field"
+                                                onChange={handleInputChange}
+                                                name="interested_field"
+                                            >
+                                                <option value={"Science"}>Science</option>
+                                                <option value={"Mathematics"}>Mathematics</option>
+                                                <option value={"IT"}>Information Technology</option>
+                                            </Select>
+                                        </FormControl>
+                                        <FormControl>
+                                            <FormLabel>
+                                                What is your highest educational level?
+                                            </FormLabel>
+                                            <Select
+                                                placeholder="Select your highest educational level"
+                                                onChange={handleInputChange}
+                                                name="max_edu_level"
+                                            >
+                                                <option value={"Ordinary Level"}>Ordinary Level</option>
+                                                <option value={"Advanced Level"}>Advanced Level Level</option>
+                                                <option value={"Diploma"}>Diploma</option>
+                                                <option value={"Degree"}>Degree</option>
+                                            </Select>
+                                        </FormControl>
+                                    </>
+                                ) : null}
+
+                                <Button
+                                    className="rounded-xl w-full"
+                                    variant="solid"
+                                    colorScheme="blue"
+                                    onClick={onSubmitHandler}
+                                    isLoading={isLoading}
+                                    loadingText="Processing"
+                                >
+                                    Register
+                                </Button>
+                            </Stack>
+                        </form>
+                    </Box>
+                </Stack>
+            </Flex>
         </>
     )
 }
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,7 +1,126 @@
+import {
+    Box, Button,
+    Flex,
+    FormControl,
+    Heading, Input,
+    InputGroup,
+    InputLeftElement, InputRightElement,
+    Stack,
+    Image,
+    useColorModeValue, chakra
+} from "@chakra-ui/react";
+import React, {useState} from "react";
+import {FaLock, FaUserAlt} from "react-icons/fa";
+import {useNavigate} from "react-router-dom";
+
+const CFaUserAlt = chakra(FaUserAlt);
+const CFaLock = chakra(FaLock);
+
+
 const SignUp = () => {
+    const [showPassword, setShowPassword] = useState(false);
+    const handleShowClick = () => setShowPassword(!showPassword);
+    const [email, setEmail] = useState("dopssaas@ghj.com");
+    const [password, setPassword] = useState("dsdfdsfsdfsf");
+    const navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(false);
+
+    const signUpHandler = async () => {
+        setIsLoading(true)
+    }
+
+
     return (
         <>
-            <h1>Sign Up</h1>
+            <Flex
+                flexDirection="column"
+                width="100wh"
+                height="100vh"
+                bg={useColorModeValue("blue.500", "blue.500")}
+                justifyContent="center"
+                alignItems="center"
+                blur={"md"}
+            >
+                <Stack className="justify-center align-center flex-col mb-2">
+                    <Box className="m-auto">
+                        {/*<Image src={logo} height={"100px"}/>*/}
+                    </Box>
+
+                    <Heading color="white">Welcome to Course Explorer</Heading>
+                    <Box minW={{base: "90%", md: "468px"}}>
+                        <Stack className="bg-white gap-4 p-8 shadow-xl rounded-xl">
+                            <FormControl isRequired className={"pb-4"}>
+                                <InputGroup>
+                                    <InputLeftElement
+                                        pointerEvents="none"
+                                        children={<CFaUserAlt color="gray.300"/>}
+                                    />
+                                    <Input
+                                        onChange={(e) => {
+                                            setEmail(e.target.value);
+                                        }}
+                                        type="email"
+                                        placeholder="email address"
+                                    />
+                                </InputGroup>
+                            </FormControl>
+                            <FormControl className={"pb-4"}>
+                                <InputGroup>
+                                    <InputLeftElement
+                                        pointerEvents="none"
+                                        color="gray.300"
+                                        children={<CFaLock color="gray.300"/>}
+                                    />
+                                    <Input
+                                        type={showPassword ? "text" : "password"}
+                                        placeholder="Password"
+                                        onChange={(e) => {
+                                            setPassword(e.target.value);
+                                        }}
+                                    />
+                                    <InputRightElement width="4.5rem">
+                                        <Button h="1.75rem" size="sm" onClick={handleShowClick}>
+                                            {showPassword ? "Hide" : "Show"}
+                                        </Button>
+                                    </InputRightElement>
+                                </InputGroup>
+                            </FormControl>
+                            <FormControl className={"pb-4"}>
+                                <InputGroup>
+                                    <InputLeftElement
+                                        pointerEvents="none"
+                                        color="gray.300"
+                                        children={<CFaLock color="gray.300"/>}
+                                    />
+                                    <Input
+                                        type={showPassword ? "text" : "password"}
+                                        placeholder="Re-enter password"
+                                        onChange={(e) => {
+                                            setPassword(e.target.value);
+                                        }}
+                                    />
+                                    <InputRightElement width="4.5rem">
+                                        <Button h="1.75rem" size="sm" onClick={handleShowClick}>
+                                            {showPassword ? "Hide" : "Show"}
+                                        </Button>
+                                    </InputRightElement>
+                                </InputGroup>
+                            </FormControl>
+                            <Button
+                                className="rounded-xl"
+                                variant="solid"
+                                colorScheme="blue"
+                                width="full"
+                                onClick={signUpHandler}
+                                isLoading={isLoading}
+                                loadingText="Signing Up"
+                            >
+                                SignUp
+                            </Button>
+                        </Stack>
+                    </Box>
+                </Stack>
+            </Flex>
         </>
     )
 }
